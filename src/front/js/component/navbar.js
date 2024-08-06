e8b839ddbe86d3593dfafcd9ccb18467457ba52d
Sonia <161872642+Inflanubes@users.noreply.github.com>
2024-08-06 21:23:22 +0000

    Navbar created

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,17 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+
+	const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/');
+    };
+
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar p-3 m-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}>
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<button className="btn btn-secondary">Classified photos</button>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+						<button className="btn btn-secondary">Photos uploaded</button>
 					</Link>
 				</div>
+				<button className="btn btn-warning btn-lg" onClick={handleLogout}>
+                    Logout
+                </button>
 			</div>
 		</nav>
 	);
